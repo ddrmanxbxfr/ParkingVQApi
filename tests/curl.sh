714275b5b3f429078fb914d26e17c27604d94e90
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-08-31 00:15:34 -0400

    ajout url de test ville qc

--- tests/curl.sh
+++ tests/curl.sh
@@ -5,3 +5,6 @@ curl -X GET -H "Content-Type: application/json" http://127.0.0.1:4711/api/parkin
 
 # Radius vide
 curl -X GET -H "Content-Type: application/json" http://127.0.0.1:4711/api/parking/10/-1/-1
+
+# Location de la ville de qc :
+curl -X GET -H "Content-Type: application/json" http://127.0.0.1:4711/api/parking/46.71585637622432/-71.38057708740234/46.824965835361034/-71.27071380615234
