e05c67134d4e6b62591f341f7b04e44254adefd5
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-09-01 18:13:40 -0400

    Ajout des tests pour calculs de rayon

--- test/calculs.js
+++ test/calculs.js
@@ -12,3 +12,21 @@ describe('Calculer la distance entre 2 points', function () {
         distanceEntreDeuxPoints.should.be.exactly(1544.7575610296099).and.be.a.Number;
     });
 });
+
+
+describe('Presence d\'un point dans le rayon', function () {
+   var rayonPlusPetitQueDistance, rayonPlusGrandQueDistance;
+    before(function (done) {
+        rayonPlusPetitQueDistance = calculs.isPointInRadius(1500, 10, 10, 20, 20);
+        rayonPlusGrandQueDistance = calculs.isPointInRadius(2000, 10, 10, 20, 20);
+        done();  
+    })
+    
+    it('should not be in radius if rad is smaller than distance', function() {
+        rayonPlusPetitQueDistance.should.be.exactly(false).and.be.a.Boolean;
+    });
+    
+    it('should be in radius if rad is bigger than distance', function() {
+        rayonPlusGrandQueDistance.should.be.exactly(true).and.be.a.Boolean;
+    });
+});
