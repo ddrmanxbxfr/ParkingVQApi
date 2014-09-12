69c024b156c86816d1ba470990e145a4cac6988a
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-09-12 16:44:06 -0400

    ajout de script pour benchmark + test curl

--- tests_bash/curl.sh
+++ tests_bash/curl.sh
@@ -6,5 +6,8 @@ curl -X GET -H "Content-Type: application/json" http://127.0.0.1:4711/api/parkin
 # Radius vide
 curl -X GET -H "Content-Type: application/json" http://127.0.0.1:4711/api/parking/10/-1/-1
 
+# Radius réel
+curl -X GET -H "Content-Type: application/json" http://127.0.0.1:4711/api/parking/1000/46.71585637622432/-71.38057708740234
+
 # Location de la ville de qc :
 curl -X GET -H "Content-Type: application/json" http://127.0.0.1:4711/api/parking/46.71585637622432/-71.38057708740234/46.824965835361034/-71.27071380615234
